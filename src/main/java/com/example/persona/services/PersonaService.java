4f85bca43c5f8bc9be7f81582e725e6208c3b0ed
NachoDaibes <nachodaibes25@gmail.com>
2021-09-14 20:01:09 -0300

    Querys agregadas

--- src/main/java/com/example/persona/services/PersonaService.java
+++ src/main/java/com/example/persona/services/PersonaService.java
@@ -2,5 +2,9 @@ package com.example.persona.services;
 
 import com.example.persona.entities.Persona;
 
+import java.util.List;
+
 public interface PersonaService extends BaseService<Persona, Long>{
+
+    List<Persona> search(String filtro) throws Exception;
 }
